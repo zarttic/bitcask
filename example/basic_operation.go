474054b5c63636c76f8e13eb40bd7977971aaf8d
zarttic <332209078@qq.com>
2024-02-15 11:48:15 +0800

    feat: Add example operation

---
+++ example/basic_operation.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	bitcask "bitcask"
+	"fmt"
+)
+
+func main() {
+	config := bitcask.DefaultConfig
+	db, err := bitcask.Open(config)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Put([]byte("key"), []byte("value"))
+	if err != nil {
+		panic(err)
+	}
+	val, err := db.Get([]byte("key"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("val = ", string(val))
+
+	err = db.Delete([]byte("key"))
+	if err != nil {
+		panic(err)
+	}
+}
