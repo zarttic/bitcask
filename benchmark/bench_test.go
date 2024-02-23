81d73215f0172d44ee47dbd052e4c4e33317a9ca
zarttic <332209078@qq.com>
2024-02-23 13:29:23 +0800

    feat: benchmark

---
+++ benchmark/bench_test.go
@@ -0,0 +1,31 @@
+package benchmark
+
+import (
+	"bitcask"
+	"bitcask/utils"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+var db *bitcask.DB
+
+func init() {
+	cfg := bitcask.DefaultConfig
+	dir, err := os.MkdirTemp("", "bitcask-bench-test")
+	if err != nil {
+		panic(err)
+	}
+	db, err = bitcask.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
+}
+func BenchmarkPut(b *testing.B) {
+	b.ResetTimer()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := db.Put(utils.GetTestKey(i), utils.GetTestValue(1024))
+		assert.Nil(b, err)
+	}
+}
