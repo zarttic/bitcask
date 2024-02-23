ab057b406c0b1a8669ec72eada0d5c21360b568c
zarttic <332209078@qq.com>
2024-02-23 14:33:56 +0800

    feat: benchmark

--- benchmark/bench_test.go
+++ benchmark/bench_test.go
@@ -3,9 +3,12 @@ package benchmark
 import (
 	"bitcask"
 	"bitcask/utils"
+	"errors"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 )
 
 var db *bitcask.DB
@@ -27,6 +30,33 @@ func Benchmark_Put(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		err := db.Put(utils.GetTestKey(i), utils.GetTestValue(1024))
+		assert.Nil(b, err)
+	}
+}
+func Benchmark_Get(b *testing.B) {
+	for i := 0; i < 10000; i++ {
+		err := db.Put(utils.GetTestKey(i), utils.GetTestValue(1024))
+		assert.Nil(b, err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := db.Get(utils.GetTestKey(rand.Int()))
+		if err != nil && !errors.Is(err, bitcask.ErrKeyNotFound) {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_Delete(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < b.N; i++ {
+		err := db.Delete(utils.GetTestKey(rand.Int()))
 		assert.Nil(b, err)
 	}
 }
